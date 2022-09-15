20f151a3c39d8fc13baabe631573ada0700e36da
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-14 22:26:22 -0700

    Update UnixLinuxInterviewQuestionsPart1.sh

--- UnixLinuxInterviewQuestionsPart1.sh
+++ UnixLinuxInterviewQuestionsPart1.sh
@@ -2,23 +2,23 @@
 echo $SHELL
 
 # How to locate where is command located?
-which nameOfTheCommand
+# which nameOfTheCommand
 
 # How to get rows from 10 to 20 in the file?
-cat emp.txt | head -20 | tail -10
+cat nameOfFile.txt | head -20 | tail -10
 
 # Get count of all the unique rows in the file?
-cat emp.txt | sort -u | wc -l
+cat nameOfFile.txt | sort -u | wc -l
 
 # Get count of all duplicate rows in the file?
-cat emp.txt | sort | uniq -d | wc -l
+cat nameOfFile.txt | sort | uniq -d | wc -l
 
 # Get count of each row in file?
-cat emp.txt | sort | uniq -c
+cat nameOfFile.txt | sort | uniq -c
 
 # What are hidden files and how to display them?
 ls -la
 
 # Difference between ls - lRT & ls -lrt?
-R -> Recursively display sub-directory
-r -> reverse order
+# R -> Recursively display sub-directory
+# r -> reverse order
