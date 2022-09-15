b477de6697ac45f81e62dac5a53665a07fe06666
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-14 22:07:44 -0700

    Create UnixLinuxInterviewQuestionsPart1.sh

---
+++ UnixLinuxInterviewQuestionsPart1.sh
@@ -0,0 +1,11 @@
+# How to get which shell you are working on?
+echo $SHELL
+
+# How to locate where is command located?
+which nameOfTheCommand
+
+# How to get rows from 10 to 20 in the file?
+cat emp.txt | head -20 | tail -10
+
+# Get the count of all the unique rows in the file?
+cat emp.txt | sort -u | wc -|
