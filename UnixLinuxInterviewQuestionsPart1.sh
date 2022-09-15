e44178a4f6d60ef02e0d86cb80a5e7ea58f9d8d8
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-14 22:25:01 -0700

    Update UnixLinuxInterviewQuestionsPart1.sh

--- UnixLinuxInterviewQuestionsPart1.sh
+++ UnixLinuxInterviewQuestionsPart1.sh
@@ -7,5 +7,18 @@ which nameOfTheCommand
 # How to get rows from 10 to 20 in the file?
 cat emp.txt | head -20 | tail -10
 
-# Get the count of all the unique rows in the file?
+# Get count of all the unique rows in the file?
 cat emp.txt | sort -u | wc -l
+
+# Get count of all duplicate rows in the file?
+cat emp.txt | sort | uniq -d | wc -l
+
+# Get count of each row in file?
+cat emp.txt | sort | uniq -c
+
+# What are hidden files and how to display them?
+ls -la
+
+# Difference between ls - lRT & ls -lrt?
+R -> Recursively display sub-directory
+r -> reverse order
