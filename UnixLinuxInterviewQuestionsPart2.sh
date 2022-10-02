4e311c1ee85b88d8d805a5b8e24d09b02a87964b
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-10-01 23:49:58 -0700

    Update UnixLinuxInterviewQuestionsPart2.sh

--- UnixLinuxInterviewQuestionsPart2.sh
+++ UnixLinuxInterviewQuestionsPart2.sh
@@ -4,3 +4,21 @@
 # Command used to kill last background job?
 Kill $!
 
+# Command used to remove 7th line from file?
+sed -i '7 d' nameOfFile.txt
+
+# Command used to change password?
+passwd
+
+# Command used to change protection mode of files beginning with string emp & finishing with 1 or 2?
+chmod u+x emp[1-3]
+
+# Command for changing Owners?
+chown
+
+# Command used to change Groups?
+chgrp
+
+# Command view username in Linux-Unix?
+whoami
+
