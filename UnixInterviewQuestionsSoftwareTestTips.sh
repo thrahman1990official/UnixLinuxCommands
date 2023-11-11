8bef43981c1ebf9f660a89cc247854e999fc66cb
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-10 22:39:53 -0800

    Update UnixInterviewQuestionsSoftwareTestTips.sh

--- UnixInterviewQuestionsSoftwareTestTips.sh
+++ UnixInterviewQuestionsSoftwareTestTips.sh
@@ -2,3 +2,7 @@
 #1) What's shell in UNIX?
 #2) How do you list files in a directory using the LS Command?
 #3) What's purpose of pwd command?
+#4) How do you create new directory in UNIX-LINUX?
+#5) Explain cd command in UNIX?
+#6) How do you copy files in UNIX?
+#7) Explain purpose of mv command?
