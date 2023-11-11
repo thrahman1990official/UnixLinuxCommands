796ac91e81c966ec4ef211db174b9d001ea84538
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-10 22:25:30 -0800

    Update UnixInterviewQuestionsSoftwareTestTips.sh

--- UnixInterviewQuestionsSoftwareTestTips.sh
+++ UnixInterviewQuestionsSoftwareTestTips.sh
@@ -1,3 +1,4 @@
 # https://www.softwaretesttips.com/unix-interview-questions/
 #1) What's shell in UNIX?
 #2) How do you list files in a directory using the LS Command?
+#3) What's purpose of pwd command?
