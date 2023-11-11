92fa639859a2ca315d6503adc68c1a04b241a2b3
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-11 02:17:16 -0800

    Update UnixInterviewQuestionsSoftwareTestTips.sh

--- UnixInterviewQuestionsSoftwareTestTips.sh
+++ UnixInterviewQuestionsSoftwareTestTips.sh
@@ -1,11 +1,15 @@
 # https://www.softwaretesttips.com/unix-interview-questions/
-#1) What's shell in UNIX?
+#1) What's shell in UNIX-LINUX?
 #2) How do you list files in a directory using the LS Command?
 #3) What's purpose of pwd command?
 #4) How do you create new directory in UNIX-LINUX?
-#5) Explain cd command in UNIX?
-#6) How do you copy files in UNIX?
+#5) Explain cd command in UNIX-LINUX?
+#6) How do you copy files in UNIX-LINUX?
 #7) Explain purpose of mv command?
-#8) How do you remove file in UNIX?
+#8) How do you remove file in UNIX-LINUX?
 #9) Purpose of rmdir command?
-#10) How do you create empty file in UNIX?
+#10) How do you create empty file in UNIX-LINUX?
+#11) Explain cat command in UNIX-LINUX?
+#12) How do you view contents of file page by page in UNIX-LINUX?
+#13) What's symbolic link in UNIX-LINUX?
+#14) Explain grep command in UNIX-LINUX?
