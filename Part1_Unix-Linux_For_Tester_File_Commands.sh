9c2ee91f6af44ee8363069a9bc6b03589db2342a
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-21 00:59:52 -0800

    Create Part1_Unix-Linux_For_Tester_File_Commands.sh

---
+++ Part1_Unix-Linux_For_Tester_File_Commands.sh
@@ -0,0 +1,9 @@
+#Below are examples of Cat commands
+#Creating new file
+cat > newFile.txt
+#Concatenating also known as Displaying contents of a file
+cat newFile.txt
+#Concatenating more than one file
+cat fileName1.txt fileName2.txt
+#Appending data to existing file
+cat >> fileName.txt
