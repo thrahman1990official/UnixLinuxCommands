602416f37607428e75ae5d4eefb622c0c008ded1
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-22 07:32:59 -0800

    Update Part2_Unix-Linux_Commands_For_Testers_Directory_Commands.sh

--- Part2_Unix-Linux_Commands_For_Testers_Directory_Commands.sh
+++ Part2_Unix-Linux_Commands_For_Testers_Directory_Commands.sh
@@ -6,3 +6,9 @@ mkdir directoryName
 mkdir directoryName1 directoryName2 directoryName3
 #command for making multiple subdirectories inside the directory, below is an example
 mkdir -p world/countries/states
+mkdir -p directoryName/subDirectoryName/subDirectoryOfSubDirectory
+
+#command for change directory or going into the next directory
+cd directoryName
+#if we use pwd command we can get all the sub-directories
+cd directoryName/subDirectoryName/subDirectoryOfSubDirectory
