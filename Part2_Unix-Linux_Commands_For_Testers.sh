9e7c688438cf6f6cc69b4af80201e03cf6af6bfa
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-22 07:25:08 -0800

    Create Part2_Unix-Linux_Commands_For_Testers.sh

---
+++ Part2_Unix-Linux_Commands_For_Testers.sh
@@ -0,0 +1,8 @@
+#Directory Commands examples below
+#We start with make directory commands
+#command for making single directory
+mkdir directoryName
+#command for making multiple directories
+mkdir directoryName1 directoryName2 directoryName3
+#command for making multiple subdirectories inside the directory
+mkdir -p world/countries/states
