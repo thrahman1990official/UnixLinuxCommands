7f815817265cd35c324144c0d53842a1999c5320
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-22 20:54:57 -0800

    Create Part4_Linux_Unix_Commands.sh

---
+++ Part4_Linux_Unix_Commands.sh
@@ -0,0 +1,5 @@
+#the default head commands displays first 10 lines
+head fileName.txt
+#but if you want to used the head command to display for example only first 5 line or first 20 lines, you use
+head -n 5 fileName.txt
+head -n 20 fileName.txt
