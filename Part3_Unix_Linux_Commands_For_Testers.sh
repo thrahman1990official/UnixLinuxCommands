15d51d0902a638194cfb2adfb6f903e99013bc8f
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-22 16:36:58 -0800

    Update Part3_Unix_Linux_Commands_For_Testers.sh

--- Part3_Unix_Linux_Commands_For_Testers.sh
+++ Part3_Unix_Linux_Commands_For_Testers.sh
@@ -6,11 +6,17 @@ touch .myHiddenFile.txt
 #below command is for display the path for the present working directory
 pwd
 
-#below is the command showing the list of files and/or folder/directories with their names only
+#below is the list command showing the list of files and/or folder/directories with their names only
 ls
-#below is command that displays list of files with other details such as owner permission, owner name, date created/modified and if it is a file it will display extensions
+#below is the list command that displays list of files with other details such as owner permission, owner name, date created/modified and if it is a file it will display extensions
 ls -l
-#below is command that displays all files and folders/directories in reverse order
+#below is the list command similar to ls -l except it is based on the size
+ls -lS
+#below is the list command that displays all files and folders/directories in reverse order
 ls -r
-#below is command that displays all files, folders/directories, sub-directories, in reverse order
+#below is the list command that displays all files, folders/directories, sub-directories, in reverse order
 ls -R
+#below is the list command based on type of extension
+ls *.extension
+#so in the case for displaying text files only, it will be
+ls *.txt
