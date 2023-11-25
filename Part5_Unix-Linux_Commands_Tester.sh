bcb2db9b169e355c0d6b2f72c097e97119958369
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-11-24 20:14:58 -0800

    Update Part5_Unix-Linux_Commands_Tester.sh

--- Part5_Unix-Linux_Commands_Tester.sh
+++ Part5_Unix-Linux_Commands_Tester.sh
@@ -6,10 +6,14 @@ whoami
 
 #unix command displays the name of the host of the computer
 hostname
-
 #unix command displays the I.P. Address of the host of the computer
 hostname -i
 
 #uptime command returns multiple details which returns the current time, which time it is active, how many users are currently running, load average time
 uptime
+
+#cal command by itself default shows you the current month calendar
+cal
+#but cal command with corresponding year 2021
+cal 2021
 
