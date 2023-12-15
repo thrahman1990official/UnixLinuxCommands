25be6f3b8240e1b2e1d6698784f2814d8e133213
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2023-12-15 00:03:35 -0800

    Create CompTIA-A-Plus-Linux-Examples.sh

---
+++ CompTIA-A-Plus-Linux-Examples.sh
@@ -0,0 +1,10 @@
+#Download files from repository site
+apt-get
+#Displays content of directory
+ls
+#Terminates process
+kill
+#Renames file and moves file from one directory to another
+mv
+#Starts new shell as another user
+su
