a98bafa236f9909d96082537fc7969446e32aa82
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-03-25 00:38:53 -0700

    Update Comptia_Linux_Prep_Basic_Linux_Tasks.sh

--- Comptia_Linux_Prep_Basic_Linux_Tasks.sh
+++ Comptia_Linux_Prep_Basic_Linux_Tasks.sh
@@ -1,1 +1,12 @@
-
+//Bash Commands
+//Echo command repeats input back to the screen
+echo "Good Morning!"
+//Should return Good Morning!
+//ls command lists content of the directory that can give options to view permissions and hidden files
+ls -a
+//above should return the contents and all hidden files
+pwd
+//above command should display the present working directory
+//cd changes current working directory to another directory
+cd /etc
+//will change my directory to the /etc directory
