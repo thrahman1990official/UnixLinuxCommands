e9a3324edb20bd45c5cbbac227338d0fa77dacdf
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-03-29 00:22:29 -0700

    Update Bash_Basics_Accessing_Data_From_File.sh

--- Bash_Basics_Accessing_Data_From_File.sh
+++ Bash_Basics_Accessing_Data_From_File.sh
@@ -5,5 +5,7 @@ grep -i error /home/yourUserName/folderNameForTheFile/fileToExtractData
 # in order to use the above command, you need for first do these steps below
 # you need to first go inside the folder where the file you want to extract data is contained
 cd /home/yourUserName/folderNameForTheFile/fileToExtractData
+#give owner permissions to yourUserName to fileToExtractData
 chown yourUserName fileToExtractData
+#give group permissions to yourUserName to fileToExtractData
 chgrp yourUserName fileToExtractData
