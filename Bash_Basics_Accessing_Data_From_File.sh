852d804211df44988cc20d4c2b0951aa9bcec0a6
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2024-03-28 23:21:24 -0700

    Update Bash_Basics_Accessing_Data_From_File.sh

--- Bash_Basics_Accessing_Data_From_File.sh
+++ Bash_Basics_Accessing_Data_From_File.sh
@@ -1,4 +1,9 @@
 #bash shell script for accessing data from a file
 
 #bash command for grabbing error messages
-grep -i error /home/yourUserName/fileNameToGetErrorMessage
+grep -i error /home/yourUserName/folderNameForTheFile/fileToExtractData
+# in order to use the above command, you need for first do these steps below
+# you need to first go inside the folder where the file you want to extract data is contained
+cd /home/yourUserName/folderNameForTheFile/fileToExtractData
+chown yourUserName fileToExtractData
+chgrp yourUserName fileToExtractData
